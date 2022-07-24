e68ed28c7a4fb8bcb1b0188e3d7bb71c954c2f54
Suman Adhikari <expertxkid@gmail.com>
2022-07-24 22:28:26 +0545

    feat: add image assets for flutter backgrounds

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import 'core/themes/theme.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,18 +16,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      themeMode: ThemeMode.system,
+      theme: ThemeManager.light,
+      darkTheme: ThemeManager.dark,
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       localizationsDelegates: const [
         AppLocalizations.delegate,
