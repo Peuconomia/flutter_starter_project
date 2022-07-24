a7ac7a734422ea10507135078f98ad1e7e607451
Suman Adhikari <expertxkid@gmail.com>
2022-07-25 01:02:16 +0545

    feat: add production app in app.dart

---
+++ lib/core/apps/app.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+import '../../main.dart';
+import '../themes/theme.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      themeMode: ThemeMode.system,
+      theme: ThemeManager.light,
+      darkTheme: ThemeManager.dark,
+      routerDelegate: appRouter.delegate(),
+      routeInformationParser: appRouter.defaultRouteParser(),
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('en'),
+      ],
+    );
+  }
+}
