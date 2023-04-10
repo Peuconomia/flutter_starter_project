56f5daf320d732b01993fc6ac70292b482ea7d6a
Suman Adhikari <expertxkid@gmail.com>
2023-04-10 09:39:34 +0545

    refactor/FSP-2: add @RoutePage() annotation in HomePage in home.dart

--- lib/features/home/presentation/widgets/pages/home.dart
+++ lib/features/home/presentation/widgets/pages/home.dart
@@ -1,9 +1,11 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 part 'home_mobile.dart';
 part 'home_tablet.dart';
 
+@RoutePage()
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
